e037abc257e95298d65a153dc218542a33197dd2
AlexHorovyi <alexhorovyi@gmail.com>
2021-09-17 00:20:16 +0300

    tab part 1

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,8 +2,32 @@ $(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"> <img src"../icons/left.svg"> </button>',
-        nextArrow: '<button type="button" class="slick-next"> <img src"../icons/right.svg"> </button>'
-
+        prevArrow: '<button type="button" class="slick-prev"> <img src="icons/left.svg"> </button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="icons/right.svg"> </button>',
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
